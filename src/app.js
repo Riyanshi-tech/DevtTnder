19e285eb75a7c2db74be82a323d7697f019cfb0e
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-11-06 22:54:58 +0530

    some server changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
-app.use((req,res) => {
-  res.send('Hello World');
+app.use("/hello", (req, res) => {
+  res.send('Hello  my World')
 }); 
+app.use("/host", (req, res) => {
+  res.send("Hello Host");
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
