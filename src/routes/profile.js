aa68db4dc88f0f0cc5d06b9dab102a749be56a1a
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-12-09 15:58:36 +0530

    change-password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -27,5 +27,27 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.status(400).send(error.message);
   }
 });
+profileRouter.patch("/profile/change-password", userAuth, async (req, res) => {
+  try {
+    const { Password, newPassword } = req.body;
+    if (!Password || !newPassword) {
+      return res.status(400).send("Old and new password are required");
+    }
 
+    if (newPassword.length < 6) {
+      return res.status(400).send("New password must be at least 6 characters");
+    }
+    const user = req.user;
+    const isMatch = await bcrypt.compare(Password, user.password);
+    if (!isMatch) {
+      return res.status(400).send("Incorrect old password");
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).send("Password updated successfully");
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = profileRouter;
